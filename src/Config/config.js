10386d0c01ea5de7b6ae58aa1c67287d80cce540
Your Name <dev.hashebur@gmail.com>
2024-03-19 22:52:08 +0600

    server

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,2 +1,3 @@
-export const Base_Url = "https://e-comers-server.vercel.app";
+export const Base_Url = "https://api.shohojdokan.com";
+// export const Base_Url = "https://e-comers-server.vercel.app";
 // export const Base_Url = "http://localhost:5000";
