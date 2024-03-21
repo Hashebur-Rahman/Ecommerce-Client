5e3ef23d7b2f1adf165d5cbe38aadabae5485ee7
Your Name <dev.hashebur@gmail.com>
2024-03-21 23:56:24 +0600

    update

--- src/components/hero-slider/HeroSliderOneSingle.js
+++ src/components/hero-slider/HeroSliderOneSingle.js
@@ -12,7 +12,7 @@ const HeroSliderOneSingle = ({ data }) => {
       <div className="container">
         <div className="row">
           <div className="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-6">
-            <div className="slider-content slider-animated-1">
+            <div className="slider-content  ">
               <h3 className="animated">{data.title}</h3>
               <h1 className="animated">{data.productTitle}</h1>
               <div
